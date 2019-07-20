4bb25ad73b822432b153244550e1d4720f2c501e
Christina Reinisch <christina.reinisch1@gmail.com>
2019-07-20 19:39:36 +0200

    added Logo, hero goodbye

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 cat src/header.html src/index.html src/footer.html > docs/index.html
 cp src/stylesheet.css docs/stylesheet.css
+cp src/logo_controlling_m.png docs/logo_controlling_m.png
+cp src/hero.jpg docs/hero.jpg
