1349554c796142f60c5e0ad52b9376365b990dcd
Albert Zak <me@albertzak.com>
2019-07-24 13:23:44 +0200

    added about page

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 cat src/header.html src/index.html src/footer.html > docs/index.html
+cat src/header.html src/ueber-uns.html src/footer.html > docs/ueber-uns.html
+
 cp src/stylesheet.css docs/stylesheet.css
 cp src/logo_controlling_m.png docs/logo_controlling_m.png
 cp src/hero.jpg docs/hero.jpg
+cp src/cfo.jpg docs/cfo.jpg
+cp src/mag-martin-buchegger.jpg docs/mag-martin-buchegger.jpg
+cp src/martin-buchegger-peter-voithofer.jpg docs/martin-buchegger-peter-voithofer.jpg
