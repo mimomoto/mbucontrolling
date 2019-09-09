f1c02f122e9fa562d632d1bc28f95864cda0e3f4
Christina Reinisch <christina.reinisch1@gmail.com>
2019-09-09 10:57:32 +0200

    added img folder

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+rm -rf docs/
+mkdir docs
+
 cat src/header.html src/index.html src/footer.html > docs/index.html
 cat src/header.html src/ueber-uns.html src/footer.html > docs/ueber-uns.html
 
 cp src/stylesheet.css docs/stylesheet.css
-cp src/logo_controlling_m.png docs/logo_controlling_m.png
-cp src/hero.jpg docs/hero.jpg
-cp src/cfo.jpg docs/cfo.jpg
-cp src/mag-martin-buchegger.jpg docs/mag-martin-buchegger.jpg
-cp src/martin-buchegger-peter-voithofer.jpg docs/martin-buchegger-peter-voithofer.jpg
-cp src/favicon.ico docs/favicon.ico
+cp src/img/logo_controlling_m.png docs/logo_controlling_m.png
+cp src/img/hero.jpg docs/hero.jpg
+cp src/img/cfo.jpg docs/cfo.jpg
+cp src/img/mag-martin-buchegger.jpg docs/mag-martin-buchegger.jpg
+cp src/img/martin-buchegger-peter-voithofer.jpg docs/martin-buchegger-peter-voithofer.jpg
+cp src/img/favicon.ico docs/favicon.ico
